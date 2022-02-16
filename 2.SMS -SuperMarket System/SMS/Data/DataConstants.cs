5dd54c0ef47166460a1c216ea3fb1b0cfa932496
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-02-16 12:32:49 +0200

    data constants for db creation added

---
+++ 2.SMS -SuperMarket System/SMS/Data/DataConstants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Data
+{
+    public class DataConstants
+    {
+        public const int UserEmailMaxLength = 100;
+
+        public const int UserNameMaxLength = 20;
+
+        public const int UserPasswordMaxLength = 64;
+
+
+        public const int IdMaxLength = 36;
+
+
+        public const int ProductNameMaxLength = 20;
+
+       // public const decimal ProductMinPrice = 0.05m;
+
+        //public const decimal ProductMaxPrice = 1000m;
+
+    }
+}
