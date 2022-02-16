dbe64dd374d93546a126236ba8af58f64f885959
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-02-16 14:33:45 +0200

    UsersController get LogIn + Register implemented

---
+++ 2.SMS -SuperMarket System/SMS/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using BasicWebServer.Server.Controllers;
+using BasicWebServer.Server.HTTP;
+
+namespace SMS.Controllers
+{
+    public class UsersController : Controller
+    {
+        public UsersController(Request request)
+             : base(request)
+        {
+        }
+
+        public Response Login()
+        {
+            if (User.IsAuthenticated)
+            {
+                return Redirect("/");
+            }
+
+            return View(new { IsAuthenticated = false }); 
+        }
+
+        public Response Register()
+        {
+            if (User.IsAuthenticated)
+            {
+                return Redirect("/");
+            }
+
+            return View(new { IsAuthenticated = false });
+        }
+    }
+}
